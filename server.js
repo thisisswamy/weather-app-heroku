cc29116f6400146ac8dd4c7f35c153de90130b0c
Swamy <thudumswamy786@gmail.com>
2022-06-23 16:15:16 +0530

    first commit

--- server.js
+++ server.js
@@ -1,15 +1,9 @@
-//Install express server
-const express = require('express');
+const express = require('express'); 
 const path = require('path');
-
 const app = express();
+app.use(express.static(__dirname + '/dist/<app-name>'));
+app.get('/*', function(req,res){
+    res.sendFile(path.join(__dirname+'/dist/<app-name>/index.html'));
+});
 
-// Serve only the static files form the dist directory
-app.use(express.static('./dist/angular-app-heroku'));
-
-app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/Example/'}),
-);
-
-// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
