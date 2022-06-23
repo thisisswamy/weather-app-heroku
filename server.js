f5a280a115cf5c4b2dd668ee7cf78e1bbbb79fcb
Swamy <thudumswamy786@gmail.com>
2022-06-23 16:29:38 +0530

    added server.js file

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express'); 
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/<app-name>'));
+app.use(express.static(__dirname + '/dist/example'));
 app.get('/*', function(req,res){
-    res.sendFile(path.join(__dirname+'/dist/<app-name>/index.html'));
+    res.sendFile(path.join(__dirname+'/dist/example/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
